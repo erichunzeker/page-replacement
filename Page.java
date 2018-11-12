169a85e251a05f70197830d5af45a73cd8345241
Eric Hunzeker <emh128.pitt.edu>
2018-11-12 09:40:01 -0500

    finished skeleton. half of it doesnt work im pretty sure lol

--- Page.java
+++ Page.java
@@ -1,10 +1,17 @@
 public class Page {
     String addr;
-    int bit;
+    int bit, dirty;
 
     public Page(String a, int b) {
         addr = a;
         bit = b;
+        dirty = 0;
+    }
+
+    public Page(String a, int r, int d) {
+        addr = a;
+        bit = r;
+        dirty = d;
     }
 
     public String getAddr() {
@@ -21,6 +28,14 @@ public class Page {
 
     public void setBit(int bit) {
         this.bit = bit;
+    }
+
+    public int getDirty() {
+        return dirty;
+    }
+
+    public void setDirty(int dirty) {
+        this.dirty = dirty;
     }
 
 
--- vmsim.java
+++ vmsim.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class vmsim {
     static int numframes = 0;
-    static int refresh = 0;
+    static int refresh;
     static String algorithm = null;
     static String trace = null;
     static int memaccess = 0;
@@ -41,7 +41,7 @@ public class vmsim {
                     break;
             }
 
-        }catch (IOException f) {
+        } catch (IOException f) {
             System.out.println("Trace File Not Found");
         }
 
@@ -58,9 +58,11 @@ public class vmsim {
 
             if(!table.contains(addr)) {
                 if(table.size() < numframes){
+                    System.out.println("hit" + "\n" + "page fault - no eviction");
                     pagefaults++;
                     table.add(addr);
                 } else {
+                    System.out.println("hit" + "\n" + "page fault - eviction");
                     pagefaults++;
                     table.remove(0);
                     table.add(addr);
@@ -73,15 +75,117 @@ public class vmsim {
     }
 
     public static void nru(BufferedReader br) throws IOException {
-        ArrayList<String> table = new ArrayList<>();
+        Page[] table = new Page[numframes];
+        int count = 0;
 
         String st;
         while ((st = br.readLine()) != null) {
 
+            String addr = st.substring(0, 5);
+            String action = st.substring(9, 10);
+
+            if(count < numframes) {
+                boolean nofault = false;
+                for(int i = 0; i < count; i++) {
+                    if(table[i].addr.equals(addr)) {
+                        nofault = true;
+                        if(action.equals("W"))
+                            table[i].dirty = 1;
+                        else
+                            table[i].bit = 1;
+                    }
+                }
+                if(!nofault) {
+                    if(action.equals("W")) {
+                        table[count] = new Page(addr, 1, 1);
+                        System.out.println("hit" + "\n" + "page fault - evict dirty");
+                    }
+                    else {
+                        table[count] = new Page(addr, 1, 0);
+                        System.out.println("hit" + "\n" + "page fault - evict clean");
+                    }
+                    count++;
+
+                    pagefaults++;
+                }
+            }
+
+            else {
+                boolean nofault = false;
+                for(Page p : table) {
+                    if(p.addr.equals(addr)) {
+                        nofault = true;
+                        if(action.equals("W"))
+                            p.dirty = 1;
+                        else
+                            p.bit = 1;
+                        break;
+                    }
+
+                }
+
+                if(!nofault) {
+                    boolean replaced = false;
+                    int second = -1;
+                    int third = -1;
+                    for(int i = 0; i < table.length; i++) {
+                        if(table[i].bit == 0 && table[i].dirty == 0) {
+                            if(action.equals("W"))
+                                table[i] = new Page(addr, 1, 1);
+                            else
+                                table[i] = new Page(addr, 1, 0);
+                            replaced = true;
+                            System.out.println("hit" + "\n" + "page fault - evict clean");
+                            pagefaults++;
+                            break;
+                        }
+                        else if(table[i].bit == 0 && table[i].dirty == 1)
+                            second = i;
+
+                        else if(table[i].bit == 1 && table[i].dirty == 0)
+                            third = i;
+                    }
+                    if(!replaced) {
+                        int index;
+
+                        if(second > -1) {
+                            index = second;
+                            System.out.println("hit" + "\n" + "page fault - evict dirty");
+                        }
+                        else if(third > -1) {
+                            index = third;
+                            System.out.println("hit" + "\n" + "page fault - evict clean");
+                        }
+                        else {
+                            index = 0;
+                            System.out.println("hit" + "\n" + "page fault - evict dirty");
+
+                        }
+
+                        if(action.equals("W"))
+                            table[index] = new Page(addr, 1, 1);
+                        else
+                            table[index] = new Page(addr, 1, 0);
+
+                        pagefaults++;
+                    }
+                }
+            }
+
+            if(action.equals("W"))
+                writes++;
             memaccess++;
+
+            if(refresh % memaccess == 0) {
+                for(int i = 0; i < count; i++) {
+                    table[i].bit = 0;
+                }
+            }
         }
 
     }
+
+
     public static void clock(BufferedReader br) throws IOException {
         Page[] clock = new Page[numframes];
         int count = 0;
@@ -178,17 +282,77 @@ public class vmsim {
                 writes++;
             memaccess++;
         }*/
-
     }
+
     public static void opt(BufferedReader br) throws IOException {
         ArrayList<String> table = new ArrayList<>();
+        ArrayList<String> mem = new ArrayList<>();
+        BufferedReader second = new BufferedReader(new FileReader(trace));
+        try {
+            second = new BufferedReader(new FileReader(trace));
+        } catch (IOException f) {
+            System.out.println("Trace File Not Found");
+        }
+
+
+        String memString;
+        while ((memString = second.readLine()) != null) {
+            String addr = memString.substring(0, 5);
+            mem.add(addr);
+
+        }
+
+        second.close();
+
 
         String st;
         while ((st = br.readLine()) != null) {
+            String addr = st.substring(0, 5);
+            String action = st.substring(9, 10);
 
+            if(!table.contains(addr)) {
+                if(table.size() < numframes){
+                    //System.out.println("hit" + "\n" + "page fault - no eviction");
+                    pagefaults++;
+                    table.add(addr);
+                } else {
+                    //System.out.println("hit" + "\n" + "page fault - eviction");
+                    pagefaults++;
+
+                    int[] index = new int[numframes];
+
+                    if(numframes + 1<mem.size())
+                        for(int i = 0; i < numframes; i++)
+                            mem.remove(0);
+
+                    for(int i = 0; i < numframes; i++){
+                        index[i] = mem.indexOf(table.get(i));
+                    }
+
+                    int max = -1;
+                    int maxIndex = 0;
+
+
+
+
+                    for(int i = 0; i < numframes; i++) {
+                        if(index[i] > max) {
+                            max = index[i];
+                            maxIndex = i;
+                        }
+                    }
+
+                    if(max == -1)
+                        table.remove(0);
+                    else
+                        table.remove(maxIndex);
+                    table.add(maxIndex, addr);
+                }
+            }
+            if(action.equals("W"))
+                writes++;
             memaccess++;
         }
-
     }
 
     public static void printlog() {
